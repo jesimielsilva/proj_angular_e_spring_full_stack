7dbbdd54a312cda0ea67a00b05753680d5897981
Jesimiel <jesimielbarbosa@gmail.com>
2023-02-10 01:43:25 -0300

    método getmapping

--- clientes/src/main/java/com/api/spring/clientes/rest/ClienteController.java
+++ clientes/src/main/java/com/api/spring/clientes/rest/ClienteController.java
@@ -5,6 +5,7 @@ import com.api.spring.clientes.model.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/api/clientes")
@@ -21,6 +22,13 @@ public class ClienteController {
     @ResponseStatus(HttpStatus.CREATED)
     public Cliente salvar(@RequestBody Cliente cliente){
         return repository.save(cliente);
+    }
+
+    @GetMapping("{id}")
+    public Cliente acharPorId(@PathVariable Integer id){
+        return repository
+                .findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 
 }
