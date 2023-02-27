8e3781fc5b9d9d6c3eb06f34d26d70ebea42fd64
Jesimiel <jesimielbarbosa@gmail.com>
2023-02-27 00:57:33 -0300

    Formulário de Serviços PARTE 2 no front

--- clientes-app/src/app/service-prestado/service-prestado-form/service-prestado-form.component.ts
+++ clientes-app/src/app/service-prestado/service-prestado-form/service-prestado-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from '../../clientes/cliente';
 import { ClientesService } from '../../clientes.service';
+import { ServicoPrestado } from '../servicoPrestado';
 
 @Component({
   selector: 'app-service-prestado-form',
@@ -10,10 +11,13 @@ import { ClientesService } from '../../clientes.service';
 export class ServicePrestadoFormComponent implements OnInit {
 
   clientes: Cliente[] = [];
+  servico!: ServicoPrestado;
 
   constructor(
     private clienteService: ClientesService
-  ) { }
+  ) {
+    this.servico = new ServicoPrestado();
+   }
 
   ngOnInit(): void {
     this.clienteService
@@ -22,7 +26,7 @@ export class ServicePrestadoFormComponent implements OnInit {
   }
 
   onSubmit(){
-    console.log("ok submit");
+    console.log(this.servico);
   }
 
 }
