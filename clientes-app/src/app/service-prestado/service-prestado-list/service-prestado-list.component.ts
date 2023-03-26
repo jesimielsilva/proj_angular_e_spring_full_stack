109dac59e8752a0872bef143324536be538f0dad
Jesimiel <jesimielbarbosa@gmail.com>
2023-03-26 18:55:23 -0300

    lista de serviço, consulta e botões

--- clientes-app/src/app/service-prestado/service-prestado-list/service-prestado-list.component.ts
+++ clientes-app/src/app/service-prestado/service-prestado-list/service-prestado-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ServicoPrestadosService } from '../../servico-prestados.service';
+import { ServicoPrestadoBusca } from './servicoPrestadoBusca';
 
 @Component({
   selector: 'app-service-prestado-list',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServicePrestadoListComponent implements OnInit {
 
-  constructor() { }
+  nome!: string;
+  mes!: number;
+  meses: number[];
+  lista!: ServicoPrestadoBusca[];
+  message!: string;
+
+  constructor(
+    private service: ServicoPrestadosService
+  ) {
+    this.meses = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+   }
 
   ngOnInit(): void {
+  }
+
+  consultar(){
+    this.service.buscar(this.nome, this.mes)
+    .subscribe(response => {
+      this.lista = response;
+      if(this.lista.length <= 0){
+        this.message = "Nenhum Registro Encontrado.";
+      }else{
+        this.message = '';
+      }
+    });
   }
 
 }
--- clientes-app/src/app/servico-prestados.service.ts
+++ clientes-app/src/app/servico-prestados.service.ts
@@ -1,8 +1,9 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ServicoPrestado } from './service-prestado/servicoPrestado';
 import { environment } from '../environments/environment';
+import { ServicoPrestadoBusca } from './service-prestado/service-prestado-list/servicoPrestadoBusca';
 
 @Injectable({
   providedIn: 'root'
@@ -15,5 +16,16 @@ export class ServicoPrestadosService {
 
   salvar(servicoPrestado: ServicoPrestado) : Observable<ServicoPrestado>{
     return this.http.post<ServicoPrestado>(`${this.apiURLBase}`, servicoPrestado);
+  }
+
+  buscar(nome: string, mes: number) : Observable<ServicoPrestadoBusca[]> {
+    
+    const httpParams = new HttpParams()
+    .set("nome", nome)
+    .set("mes", mes ? mes.toString() : '');     
+    
+    const url = this.apiURLBase + "?" + httpParams.toString();    
+    return this.http.get<any>(url);
+
   }
 }
